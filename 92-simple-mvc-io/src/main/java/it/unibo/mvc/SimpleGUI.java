f0b8a68050bbfb474052534d62e770cc78d6a180
Castorri Lorenzo <loricastorri@gmail.com>
2025-11-18 11:43:02 +0100

    92: First half of 'SimpleGUI.java' class implemented. Build successful.

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,13 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +15,46 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 4;
+
+    private final JFrame frame = new JFrame("SimpleGUI");
+
+    /**
+     * Constructor: Prepares the SimpleGUI object.
+     */
+    public SimpleGUI() {
+        final JPanel canvas = new JPanel(new BorderLayout());
+
+        final JTextArea txt = new JTextArea();
+        canvas.add(txt, BorderLayout.CENTER);
+        final JButton btnSave = new JButton("Save");
+        canvas.add(btnSave, BorderLayout.SOUTH);
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Sets up the frame and makes it visible.
+     */
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int newWidth = screen.width / PROPORTION;
+        final int newHeight = screen.height / PROPORTION;
+
+        frame.setSize(new Dimension(newWidth, newHeight));
+        frame.setLocationByPlatform(true);
+
+        frame.setVisible(true);
+    }
+
+    /**
+     * Starts the GUI by making an object and displaying it.
+     * 
+     * @param args ignored.
+     */
+    public static void main(final String... args) {
+        new SimpleGUI().display();
+    }
 
 }
